09a8010d7fd2d90a2a1a1d94833dd0e3d832a078
Darina <37955869+dagenator@users.noreply.github.com>
2021-12-14 15:32:04 +0500

    Js changed

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 let offset = 0;
 const slider_line = document.querySelector('.slider_line');
-let timerId = setInterval(slideMove, 4000);
+const slide_box = document.querySelector('.slide_box1');
+const slide_box2 = document.querySelector('.slide_box2');
+let window_width = document.documentElement.clientWidth;
+
+window.addEventListener("load", function(event) {
+    window_width = document.documentElement.clientWidth;
+    slide_box.style.width = window_width +"px"; 
+    slider_line.style.width =  window_width*2 +"px";
+  });
 
 
+
+window.addEventListener(`resize`, event => {
+    console.log(document.documentElement.clientWidth);
+    window_width = document.documentElement.clientWidth;
+    slide_box.style.width = window_width +"px"; 
+  }, false);
+  
+
+  let timerId = setInterval(slideMove, 4000);
 function slideMove() {
-    if(offset >= 1440){
+    //alert( window_width);
+    if(offset >=window_width){
         offset =0;
     }
     else{
-        offset+=1600;
+        offset +=window_width;
     }
     slider_line.style.left = -offset+'px';
   }
