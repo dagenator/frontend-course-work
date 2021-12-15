a7f3bfcddc780317a7d1953b137859b8eb819bde
Darina <37955869+dagenator@users.noreply.github.com>
2021-12-15 15:30:41 +0500

    slider button clicking added

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let slideNum = 3;
 let i=0;
 let waitTime=3000;
 
+// document.getElementById("slide_button1").setAttribute('onclick', function() { i= 2});
+// document.getElementById("slide_button2").setAttribute('onclick', function() { i= 1});
+
+
+
 window.addEventListener("load", function(event) {
     window_width = document.documentElement.clientWidth;
     slide_box1.style.width = window_width +"px"; 
@@ -16,15 +21,38 @@ window.addEventListener("load", function(event) {
     slider_line.style.width =  window_width*slideNum +"px";
     document.getElementById("slide_button1").className = "active_button";
     document.getElementById("slide_button2").className = "passive_button";
-  
-    slideMove();
+    document.getElementById("slide_button1").onclick = function() { 
+      i = 0;
+      console.log(i); 
+      clearTimeout(timerId); 
+      slideMove(1);
+    };
+    document.getElementById("slide_button2").onclick = function() {
+      i = 1; 
+      console.log(i); 
+      clearTimeout(timerId);
+      slideMove(0);
+    };
+
+    // document.getElementById("slide_button1").setAttribute('onclick', function() { i= 2});
+    // document.getElementById("slide_button2").setAttribute('onclick', function() { i= 1});
+
+    slideMove(0);
     
-    });
+});
+//alert("a")
+// function slide_button1(){
+//     console.log(" slide_button1");
+//     i=2;
+// }
 
-
+// function slide_button2(){
+//   console.log(" slide_button2");
+//   i=1;
+// }
 
 window.addEventListener(`resize`, event => {
-    console.log(document.documentElement.clientWidth);
+    //console.log(document.documentElement.clientWidth);
     window_width = document.documentElement.clientWidth;
     slide_box1.style.width = window_width +"px"; 
     slide_box2.style.width = window_width +"px"; 
@@ -35,29 +63,40 @@ window.addEventListener(`resize`, event => {
 
 //let timerId = setInterval(slideMove, waitTime);
 let timerId;
-function slideMove() {
+function slideMove(param) {
     if(i == 0){
-      slider_line.style.transition ="all 0s ease 0s";
+      if(param == 0){
+        slider_line.style.transition ="all 0s ease 0s";
+      }
+      else{
+        slider_line.style.transition ="all ease 1s";
+      }
       offset = window_width*i;
-      console.log(i+ " "+ -offset+'px' );
+      //console.log(i+ " "+ -offset+'px' );
       slider_line.style.left = -offset+'px';
       i++;
-      timerId = setTimeout(slideMove, waitTime/2)
+      document.getElementById("slide_button1").className = "active_button";
+      document.getElementById("slide_button2").className = "passive_button";
+      timerId = setTimeout(slideMove, waitTime/2,0)
     }
     else if(i == 1){
       slider_line.style.transition ="all ease 1s";
       offset = window_width*i;
-      console.log(i+ " "+ -offset+'px' );
+      //console.log(i+ " "+ -offset+'px' );
       slider_line.style.left = -offset+'px';
       i++;
-      timerId = setTimeout(slideMove, waitTime)
+      document.getElementById("slide_button2").className = "active_button";
+      document.getElementById("slide_button1").className = "passive_button";
+      timerId = setTimeout(slideMove, waitTime,0)
     }
     else if(i == 2){
       offset = window_width*i;
-      console.log(i+ " "+ -offset+'px' );
+      //console.log(i+ " "+ -offset+'px' );
       slider_line.style.left = -offset+'px';
       i = 0;
-      timerId = setTimeout(slideMove, waitTime/2)
+      document.getElementById("slide_button1").className = "active_button";
+      document.getElementById("slide_button2").className = "passive_button";
+      timerId = setTimeout(slideMove, waitTime/2,0)
       
     }
     
