00783dc42add8223c20293b882242ca764e2f9b4
exceq <mihail.petuxov@yandex.ru>
2021-12-15 19:06:21 +0500

    Вынес шрифты в отдельный .css, добавил окно авторизации в админку

--- script.js
+++ script.js
@@ -34,11 +34,6 @@ window.addEventListener("load", function(event) {
       clearTimeout(timerId);
       slideMove(0);
     };
-
-    document.getElementById("top_header_yellow_square").onclick = function(){hide()};
-    document.getElementById("modal_exit_button").onclick = function(){hide()};
-    document.getElementById("modal_enter").onclick = function(){hide()};
-    
     slideMove(0);
     
 });
